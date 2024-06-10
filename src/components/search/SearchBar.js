da9dbcd17c4387c3e60d363d265f7a170c8cf47a
abhinand-vp <abhinandvpellath@gmail.com>
2024-06-10 09:14:12 +0530

    search suggestion ui

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -4,7 +4,8 @@ import { useState } from "react"
 
 const SearchBar = (props) => {
     const [showAndHideSearchSuggestion, setShowAndHideSearchSuggestion] = useState(false)
-    const { classname, placeHolder } = props
+    const { classname, placeHolder, serachModalWidth } = props
+    console.log({ serachModalWidth });
     const { topDestDetails, popularDestination, handleClear, setTopDestDetails, setPopularDestination } = useDestContext()
     const handleSearch = (e) => {
         if (!e.target.value?.trim()) {
@@ -32,8 +33,8 @@ const SearchBar = (props) => {
 
     return (
         <>
-            <form className="hidden md:block md:mx-auto md:w-[30vw] relative">
-                <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only bg-white">Search</label>
+            <form className="md:block md:mx-auto md:w-[30vw] relative">
+                {/* <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only bg-white">Search</label> */}
                 <div className="relative">
                     <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                         <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -44,10 +45,10 @@ const SearchBar = (props) => {
                 </div>
             </form>
             {showAndHideSearchSuggestion &&
-                <div className="bg-white w-[30vw] border-b border-l border-r border-gray-300 z-10 h-48 absolute top-10 p-5 flex gap-x-3 gap-y-1 flex-wrap rounded-b-md">
-                    <div className="border border-gray-300 p-2 w-auto h-10 rounded-md hover:border-blue-300 hover:border-2 cursor-pointer">Dubai</div>
+                <div className={`bg-white w-[95vw] md:w-[${[serachModalWidth]}] border-b border-l border-r border-gray-300 z-10 h-48 absolute top-10 p-5 flex gap-x-3 flex-wrap rounded-b-md`}>
                     <div className="border border-gray-300 p-2 w-auto h-10 rounded-md hover:border-blue-300 hover:border-2 cursor-pointer">Burj Khalifa</div>
                     <div className="border border-gray-300 p-2 w-auto h-10 rounded-md hover:border-blue-300 hover:border-2 cursor-pointer">IMG Worlds of Adventure</div>
+                    <div className="border border-gray-300 p-2 w-auto h-10 rounded-md hover:border-blue-300 hover:border-2 cursor-pointer">Dubai</div>
                     <div className="border border-gray-300 p-2 w-auto h-10 rounded-md hover:border-blue-300 hover:border-2 cursor-pointer">Aquaventure Waterpark</div>
                 </div>
             }
